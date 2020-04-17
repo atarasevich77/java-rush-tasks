7097aea9d1d9a65e21bc119589004121cb248764
kanokenji <aleksandrtarsevich77@gmail.com>
2020-04-17 11:08:44 -0600

    add tasks of 14th Lesson

---
+++ 2.JavaCore/src/com/javarush/task/task14/task1411/Solution.java
@@ -0,0 +1,68 @@
+package com.javarush.task.task14.task1411;
+
+import java.io.BufferedReader;
+import java.io.InputStreamReader;
+import java.util.*;
+
+/* 
+User, Loser, Coder and Proger
+1. Метод main должен считывать строки с клавиатуры.
+2. Метод main должен прекращать считывать строки с клавиатуры, как только введенная строка не совпадает с одной из ожидаемых(user, loser, coder, proger).
+3. Для каждой корректной(user, loser, coder, proger) введенной строки должен быть вызван метод doWork с соответствующим объектом класса Person в качестве параметра.
+4. В классе Solution должен быть реализован метод doWork с одним параметром типа Person.
+5. Метод doWork должен вызывать метод live() у переданного объекта, если этот объект имеет тип User.
+6. Метод doWork должен вызывать метод doNothing() у переданного объекта, если этот объект имеет тип Loser.
+7. Метод doWork должен вызывать метод writeCode() у переданного объекта, если этот объект имеет тип Coder.
+8. Метод doWork должен вызывать метод enjoy() у переданного объекта, если этот объект имеет тип Proger.
+*/
+
+public class Solution {
+    public static void main(String[] args) throws Exception {
+        BufferedReader reader = new BufferedReader(new InputStreamReader(System.in));
+        Person person = null;
+        String key = null;
+
+        List<String> list = new ArrayList<>();
+        Collections.addAll(list, "user", "loser", "coder", "proger");
+        //тут цикл по чтению ключей, пункт 1
+        while (true) {
+            key = reader.readLine();
+            if(!list.contains(key)) break;
+            //создаем объект, пункт 2
+            switch (key){
+                case "user":
+                    person = new Person.User();
+                    break;
+                case "loser":
+                    person = new Person.Loser();
+                    break;
+                case "coder":
+                    person = new Person.Coder();
+                    break;
+                case "proger":
+                    person = new Person.Proger();
+                    break;
+                default:
+                    break;
+            }
+            doWork(person); //вызываем doWork
+        }
+        reader.close();
+    }
+
+    public static void doWork(Person person) {
+        // пункт 3
+        if(person instanceof Person.User) {
+            ((Person.User) person).live();
+        }
+        if (person instanceof Person.Loser) {
+            ((Person.Loser) person).doNothing();
+        }
+        if (person instanceof Person.Coder) {
+            ((Person.Coder) person).writeCode();
+        }
+        if (person instanceof Person.Proger) {
+            ((Person.Proger) person).enjoy();
+        }
+    }
+}
