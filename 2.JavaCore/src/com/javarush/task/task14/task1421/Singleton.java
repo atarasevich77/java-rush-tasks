5180953f2b2961d6eeee3bd5ecb909aa0f7c0150
kanokenji <aleksandrtarsevich77@gmail.com>
2020-04-21 11:56:44 -0600

    add tasks of 14th Lesson

--- 2.JavaCore/src/com/javarush/task/task14/task1421/Singleton.java
+++ 2.JavaCore/src/com/javarush/task/task14/task1421/Singleton.java
@@ -1,6 +1,7 @@
 package com.javarush.task.task14.task1421;
 
 public class Singleton {
+    //multi threads realisation
     private static Singleton instance = new Singleton();
 
     private Singleton() {
