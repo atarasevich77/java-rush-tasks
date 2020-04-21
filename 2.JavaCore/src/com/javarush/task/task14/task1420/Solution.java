20f9cb75a7017b326ee7445f0de74e284ea230b9
kanokenji <aleksandrtarsevich77@gmail.com>
2020-04-21 10:33:47 -0600

    add tasks of 14th Lesson

--- 2.JavaCore/src/com/javarush/task/task14/task1420/Solution.java
+++ 2.JavaCore/src/com/javarush/task/task14/task1420/Solution.java
@@ -12,9 +12,26 @@ package com.javarush.task.task14.task1420;
 4. Программа должна выводить на экран наибольший общий делитель(НОД) чисел считанных с клавиатуры и успешно завершаться.
 */
 
+import java.io.BufferedReader;
+import java.io.InputStreamReader;
+
 public class Solution {
+
     public static void main(String[] args) throws Exception {
+        BufferedReader reader = new BufferedReader(new InputStreamReader(System.in));
+        int num1 = Integer.parseInt(reader.readLine());
+        int num2 = Integer.parseInt(reader.readLine());
 
+        if(num1 <= 0 || num2 <= 0) {
+            throw new Exception();
+        }
 
+        int temp;
+        while(num2 != 0){
+            temp = num2;
+            num2 = num1 % num2;
+            num1 = temp;
+        }
+        System.out.println(num1);
     }
 }
