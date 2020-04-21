6df0fcc83b45df5d723a1e2dc7504fb5cb367ade
kanokenji <aleksandrtarsevich77@gmail.com>
2020-04-21 13:32:24 -0600

    add tasks of 15th Lesson

--- 2.JavaCore/src/com/javarush/task/task15/task1501/Solution.java
+++ 2.JavaCore/src/com/javarush/task/task15/task1501/Solution.java
@@ -2,6 +2,12 @@ package com.javarush.task.task15.task1501;
 
 /* 
 ООП - Расставить интерфейсы
+1. Класс Clothes должен поддерживать интерфейс Movable.
+2. Класс Clothes должен поддерживать интерфейс Sellable.
+3. Класс Clothes должен поддерживать интерфейс Discountable.
+4. В классе Clothes должен быть реализован метод isMovable.
+5. В классе Clothes должен быть реализован метод getAllowedAction без параметров.
+6. В классе Clothes должен быть реализован метод getAllowedAction с одним параметром типа String.
 */
 
 public class Solution {
@@ -21,6 +27,20 @@ public class Solution {
         Object getAllowedAction();
     }
 
-    public static class Clothes {
+    public static class Clothes implements Discountable, Movable, Sellable {
+        @Override
+        public boolean isMovable() {
+            return false;
+        }
+
+        @Override
+        public Object getAllowedAction() {
+            return null;
+        }
+
+        @Override
+        public Object getAllowedAction(String name) {
+            return null;
+        }
     }
 }
