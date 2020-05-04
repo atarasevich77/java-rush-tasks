45aca611b63befedcb09a9ad773a205f5827e77a
kanokenji <aleksandrtarsevich77@gmail.com>
2020-05-04 13:41:43 -0600

    add tasks of 18th Lesson (input and output streams)

--- 2.JavaCore/src/com/javarush/task/task18/task1812/QuestionFileOutputStream.java
+++ 2.JavaCore/src/com/javarush/task/task18/task1812/QuestionFileOutputStream.java
@@ -4,7 +4,48 @@ package com.javarush.task.task18.task1812;
 Расширяем AmigoOutputStream
 */
 
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+
 public class QuestionFileOutputStream implements AmigoOutputStream {
 
+    private AmigoOutputStream aos;
+
+    public QuestionFileOutputStream(AmigoOutputStream aos) {
+        this.aos = aos;
+    }
+
+    @Override
+    public void flush() throws IOException {
+        aos.flush();
+    }
+
+    @Override
+    public void write(int b) throws IOException {
+        aos.write(b);
+    }
+
+    @Override
+    public void write(byte[] b) throws IOException {
+        aos.write(b);
+    }
+
+    @Override
+    public void write(byte[] b, int off, int len) throws IOException {
+        aos.write(b, off, len);
+    }
+
+    @Override
+    public void close() throws IOException {
+        System.out.println("Вы действительно хотите закрыть поток? Д/Н");
+        try(BufferedReader reader = new BufferedReader(new InputStreamReader(System.in))){
+            String str = reader.readLine();
+            if(str.equals("Д")){
+                aos.close();
+            }
+        } catch(Exception e){
+        }
+    }
 }
 
