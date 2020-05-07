6d1de50ae90de2f6f1774ccae6afb532f97ed6c8
kanokenji <aleksandrtarsevich77@gmail.com>
2020-05-07 14:56:21 -0600

    add tasks of 19th Lesson (pattern Adapter)

--- 2.JavaCore/src/com/javarush/task/task19/task1902/AdapterFileOutputStream.java
+++ 2.JavaCore/src/com/javarush/task/task19/task1902/AdapterFileOutputStream.java
@@ -4,12 +4,35 @@ package com.javarush.task.task19.task1902;
 Адаптер
 */
 
-public class AdapterFileOutputStream {
+import java.io.*;
+
+public class AdapterFileOutputStream implements AmigoStringWriter {
+
+    private FileOutputStream fileOutputStream;
+    public AdapterFileOutputStream(FileOutputStream fileOutputStream){
+        this.fileOutputStream = fileOutputStream;
+    }
 
     public static void main(String[] args) {
 
     }
 
+    public void writeString(String s) throws IOException {
+        fileOutputStream.write(s.getBytes());
+    }
 
+    @Override
+    public void close() throws IOException {
+        fileOutputStream.close();
+    }
+
+    @Override
+    public void flush() throws IOException {
+        fileOutputStream.flush();
+    }
+
+    public interface AmigoStringWriter {
+
+    }
 }
 
