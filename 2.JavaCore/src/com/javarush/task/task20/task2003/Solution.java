fb0418ed4ca65c3a33c7db7049af0e699f756cf4
kanokenji <aleksandrtarsevich77@gmail.com>
2020-05-12 12:16:55 -0600

    add tasks of 20th Lesson (Save Objects/array of Objects to file)

--- 2.JavaCore/src/com/javarush/task/task20/task2003/Solution.java
+++ 2.JavaCore/src/com/javarush/task/task20/task2003/Solution.java
@@ -1,11 +1,11 @@
 package com.javarush.task.task20.task2003;
 
 import java.io.*;
-import java.util.HashMap;
-import java.util.Map;
+import java.util.*;
 
 /* 
 Знакомство с properties
+C:\Users\vika\Downloads\files_for_tests\16.properties
 */
 public class Solution {
 
@@ -13,10 +13,16 @@ public class Solution {
 
     public static void save(OutputStream outputStream) throws Exception {
         //напишите тут ваш код
+        Properties properties = new Properties();
+        properties.putAll(runtimeStorage);
+        properties.store(outputStream, "");
     }
 
     public static void load(InputStream inputStream) throws IOException {
         //напишите тут ваш код
+        Properties properties = new Properties();
+        properties.load(inputStream);
+        properties.forEach((k,v)->runtimeStorage.put(k.toString(),v.toString()));
     }
 
     public static void main(String[] args) {
