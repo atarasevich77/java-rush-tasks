c555f0171e6d67996e092034caa4e5f3eed71cc1
kanokenji <aleksandrtarsevich77@gmail.com>
2020-05-12 15:05:15 -0600

    add tasks of 20th Lesson (Save Objects/array of Objects to file and serilalize)

---
+++ 2.JavaCore/src/com/javarush/task/task20/task2021/Solution.java
@@ -0,0 +1,27 @@
+package com.javarush.task.task20.task2021;
+
+import java.io.*;
+
+/*
+Сериализация под запретом
+1. Класс Solution должен поддерживать интерфейс Serializable.
+2. Класс SubSolution должен быть создан внутри класса Solution.
+3. Класс SubSolution должен быть потомком класса Solution.
+4. При попытке сериализовать объект типа SubSolution должно возникать исключение NotSerializableException.
+5. При попытке десериализовать объект типа SubSolution должно возникать исключение NotSerializableException.
+*/
+public class Solution implements Serializable {
+    public static class SubSolution extends Solution {
+        private void writeObject(ObjectOutputStream stream) throws IOException {
+            throw new NotSerializableException("!!!");
+        }
+
+        private void readObject(ObjectInputStream stream) throws IOException, ClassNotFoundException {
+            throw new NotSerializableException("!!!");
+        }
+    }
+
+    public static void main(String[] args) {
+
+    }
+}
