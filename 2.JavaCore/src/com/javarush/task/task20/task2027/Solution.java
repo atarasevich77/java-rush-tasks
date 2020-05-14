1033164b6f7b3bf9e3f8ea8b3980a5df1156eda9
kanokenji <aleksandrtarsevich77@gmail.com>
2020-05-14 15:16:37 -0600

    Lesson 20th - serialization

--- 2.JavaCore/src/com/javarush/task/task20/task2027/Solution.java
+++ 2.JavaCore/src/com/javarush/task/task20/task2027/Solution.java
@@ -1,6 +1,6 @@
 package com.javarush.task.task20.task2027;
 
-import java.util.List;
+import java.util.*;
 
 /* 
 Кроссворд
@@ -14,17 +14,58 @@ public class Solution {
                 {'m', 'l', 'p', 'r', 'r', 'h'},
                 {'p', 'o', 'e', 'e', 'j', 'j'}
         };
-        detectAllWords(crossword, "home", "same");
+        System.out.println(detectAllWords(crossword, "home", "same"));
         /*
 Ожидаемый результат
 home - (5, 3) - (2, 0)
 same - (1, 1) - (4, 1)
          */
     }
-
+    private static boolean ok(int x, int y, int[][] crossword) {
+        return 0 <= x && x < crossword.length && 0 <= y && y < crossword[x].length;
+    }
+    private static void go(int startX, int startY, int x, int y, int dx, int dy, StringBuilder curr, int[][] crossword,
+                           HashMap<String, int[]> wordsToDetect, HashSet<String> prefixes) {
+//        System.err.println(String.format("go from (%d %d) to (%d %d)", startX, startY, x, y));
+        curr.append((char)crossword[x][y]);
+        String str = curr.toString();
+        if (prefixes.contains(str)) {
+            if (wordsToDetect.containsKey(str) && wordsToDetect.get(str)[0] == -1) {
+                wordsToDetect.put(str, new int[]{startX, startY, x, y});
+            }
+            if (ok(x + dx, y + dy, crossword)) {
+                go(startX, startY, x + dx, y + dy, dx, dy, curr, crossword, wordsToDetect, prefixes);
+            }
+        }
+        curr.setLength(curr.length() - 1);
+    }
     public static List<Word> detectAllWords(int[][] crossword, String... words) {
-
-        return null;
+        HashMap<String, int[]> wordsToDetect = new HashMap<>();
+        HashSet<String>prefixes = new HashSet<>();
+        for (String word : words) {
+            StringBuilder str = new StringBuilder();
+            for (int i = 0; i < word.length(); ++i) {
+                str.append(word.charAt(i));
+                prefixes.add(str.toString());
+            }
+            wordsToDetect.put(word, new int[]{-1, -1, -1, -1});
+        }
+        int[][] delta = {{0, 1}, {0, -1}, {1, 0}, {-1, 0}, {1, 1}, {-1, -1}, {1, -1}, {-1, 1}};
+        for (int i = 0; i < crossword.length; ++i) {
+            for (int j = 0; j < crossword[i].length; ++j) {
+                for (int[] d : delta) {
+                    go(i, j, i, j, d[0], d[1], new StringBuilder(), crossword, wordsToDetect, prefixes);
+                }
+            }
+        }
+        List<Word> ans = new ArrayList<>();
+        wordsToDetect.forEach((k, v) -> {
+            Word word = new Word(k);
+            word.setStartPoint(v[1], v[0]);
+            word.setEndPoint(v[3], v[2]);
+            ans.add(word);
+        });
+        return ans;
     }
 
     public static class Word {
