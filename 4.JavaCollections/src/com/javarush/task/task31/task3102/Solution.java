af1414237d4f9b64f82791b61c70a46d78170745
kanokenji <aleksandrtarsevich77@gmail.com>
2020-05-19 10:37:30 -0600

    Lesson 21th - Files

--- 4.JavaCollections/src/com/javarush/task/task31/task3102/Solution.java
+++ 4.JavaCollections/src/com/javarush/task/task31/task3102/Solution.java
@@ -1,18 +1,37 @@
 package com.javarush.task.task31.task3102;
 
-import java.io.IOException;
-import java.util.List;
+import java.io.*;
+import java.nio.file.*;
+import java.util.*;
+import java.util.stream.Collectors;
+
 
 /* 
 Находим все файлы
 */
 public class Solution {
     public static List<String> getFileTree(String root) throws IOException {
-        return null;
+        File folder = new File(root);
+        List<String> result = new ArrayList<>();
+        Queue<File> fileTree = new PriorityQueue<>();
 
+        Collections.addAll(fileTree, folder.listFiles());
+
+        while (!fileTree.isEmpty()) {
+            File currentFile = fileTree.remove();
+            if(currentFile.isDirectory()) {
+                Collections.addAll(fileTree, currentFile.listFiles());
+            } else {
+                result.add(currentFile.getAbsolutePath());
+            }
+        }
+        return result;
     }
 
-    public static void main(String[] args) {
-
+    public static void main(String[] args) throws IOException {
+//        Solution sol = new Solution();
+//        List<String> list = new ArrayList<>();
+//        list = sol.getFileTree("C:\\Users\\vika\\Downloads\\files_for_tests");
+//        list.forEach(System.out::println);
     }
 }
